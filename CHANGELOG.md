4202bcfd9cc1ff8e610035016ad9baac8a153708
luoluh <1327973878@qq.com>
2025-05-25 20:28:51 +0800

    v2 origin_task2_ok

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+#### 开一个git修改记录
+
+v0 初始 
+
+```
+但是画的线超过了
+```
+
+v1 初始第一个能运行第二个运行不了
+
+```
+画的线正常了
+```
+
+v2 
+
+```
+开始调第二个
+movebasenode 加了buffer的线程
+movebaselaunch 加了movebase
+把cleaning。costmap。param和另一个的costmap。common的配置的膨胀半径和机器人半径和movebase统一
+
+没解决膨胀半径大的害怕的问题 ，感觉作者那里有点蠢。
+但是先整体调整规划路径吧
+```
+
--- src/UsingMovebaseToCleanNode.cpp
+++ src/UsingMovebaseToCleanNode.cpp
@@ -12,41 +12,47 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "cleaning_using_movebase");
 
-  //tell the action client that we want to spin a thread by default
+  // tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
 
-  //wait for the action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
+  // wait for the action server to come up
+  while (!ac.waitForServer(ros::Duration(5.0)))
+  {
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
   move_base_msgs::MoveBaseGoal nextGoal;
 
-  //load the global path.
+  // load the global path.
   tf2_ros::Buffer tf(ros::Duration(10));
+  tf.setUsingDedicatedThread(true);
+  tf2_ros::TransformListener tf_listener(tf);
+
   costmap_2d::Costmap2DROS lcr("cleaning_costmap", tf);
   CleaningPathPlanning *pathPlanner = new CleaningPathPlanning(&lcr);
 
-  //full coverage path.
+  // full coverage path.
   std::vector<geometry_msgs::PoseStamped> fullCoverPath = pathPlanner->GetPathInROS();
   int beginNum = fullCoverPath.size();
 
-  //border tracing path.
-//  std::vector<geometry_msgs::PoseStamped> borderTrackingPath = pathPlanner->GetBorderTrackingPathInROS();
-//  for(int i = 0;i<borderTrackingPath.size();i++)
-//  {
-//      fullCoverPath.push_back(borderTrackingPath[i]);
-//  }
+  // border tracing path.
+  //  std::vector<geometry_msgs::PoseStamped> borderTrackingPath = pathPlanner->GetBorderTrackingPathInROS();
+  //  for(int i = 0;i<borderTrackingPath.size();i++)
+  //  {
+  //      fullCoverPath.push_back(borderTrackingPath[i]);
+  //  }
 
-  //main loop
+  // main loop
   ros::Rate r(10);
-  for(int i = 0; i < fullCoverPath.size(); i++)
+  for (int i = 0; i < fullCoverPath.size(); i++)
   {
-      nextGoal.target_pose.header.frame_id = "map";
-      nextGoal.target_pose.header.stamp = ros::Time::now();
+
+    /*  nextGoal.target_pose.header.frame_id = "map";
+      nextGoal.target_pose.header.stamp = ros::Time(0);
 
       geometry_msgs::PoseStamped posestamped = fullCoverPath[i];
 
@@ -58,27 +64,35 @@ int main(int argc, char** argv){
       nextGoal.target_pose.pose.orientation.x = posestamped.pose.orientation.x;
       nextGoal.target_pose.pose.orientation.y = posestamped.pose.orientation.y;
       nextGoal.target_pose.pose.orientation.z = posestamped.pose.orientation.z;
+*/
+    geometry_msgs::PoseStamped posestamped = fullCoverPath[i];
+    // 强制覆盖时间戳和坐标系，避免TF查询时出错
+    posestamped.header.stamp = ros::Time(0);
+    posestamped.header.frame_id = "map";
+    nextGoal.target_pose = posestamped;
+    // 直接整体赋值给 nextGoal.target_pose，避免只赋值部分成员造成的时间戳混乱
+    nextGoal.target_pose = posestamped;
 
-      ROS_INFO("Sending next goal!");
-      ac.sendGoal(nextGoal);
-      ac.waitForResult();
+    ROS_INFO("Sending next goal!");
+    ac.sendGoal(nextGoal);
+    ac.waitForResult();
 
-      if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-      {
-          ROS_INFO("Hooray, the base moved a point forward in full path!");
-          pathPlanner->SetCoveredGrid(posestamped.pose.position.x,posestamped.pose.position.y);
-          pathPlanner->PublishGrid();
-      }
-      else
-      {
-          ROS_INFO("The base failed to move forward to the next path for some reason!");
-          continue;
-      }
-
-      pathPlanner->PublishCoveragePath();
-      ros::spinOnce();
-      r.sleep();
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+      ROS_INFO("Hooray, the base moved a point forward in full path!");
+      pathPlanner->SetCoveredGrid(posestamped.pose.position.x, posestamped.pose.position.y);
+      pathPlanner->PublishGrid();
     }
+    else
+    {
+      ROS_INFO("The base failed to move forward to the next path for some reason!");
+      continue;
+    }
+
+    pathPlanner->PublishCoveragePath();
+    ros::spinOnce();
+    r.sleep();
+  }
 
   delete pathPlanner;
   return 0;
